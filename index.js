c474fb1ccd11d0237db4c18657f9054048897c26
Alex Figar <alexfigar@me.com>
2020-09-09 20:31:51 +1000

    updated packages

--- index.js
+++ index.js
@@ -1,60 +1,69 @@
 const Discord = require('discord.js');
-const externalip = require('externalip');
+const readline = require('readline');
+const publicIp = require('public-ip');
 const cfg = require('./config.json');
 const servers = require('./servers.json');
 const ports = require('./ports.json');
 const server = require('./server.js')
 const fs = require('fs');
+const rcon = require('rcon');
 var os = require('os');
 
 const bot = new Discord.Client();
 var ipadd = '69.69.69.69';
 var serverList;
 var portList;
- 
 
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
 //On Startup
 bot.login(cfg.token);
 
-bot.on('ready', function(){
+bot.on('ready', function () {
+
     console.log("Servers Active:");
     serverList = [cfg.numberOfServers];
     portList = [cfg.numberOfServers];
     var i;
     for (i = 0; i < cfg.numberOfServers; i++) {
-        serverList[i] = servers["server"+(i+1)];
-        portList[i] = ports["server"+(i+1)];
+        serverList[i] = servers["server" + (i + 1)];
+        portList[i] = ports["server" + (i + 1)];
         console.log(serverList[i]);
     }
 
-    externalip(function (err, ip) {
+
+    publicIp.v4().then(ip => {
         ipadd = (ip); // => 8.8.8.8
       });
-        console.log("===================")
-        console.log("Ready");
+
+    console.log("===================")
+    console.log("Ready");
 });
 
 
 
 //On Message
-bot.on('message', function(message){
+bot.on('message', function (message) {
     var msg = message.content.toString();
     msg = msg.toLowerCase();
 
     var x;
     //Turkey Commands
-    if(msg.startsWith("turkey")){
-        
+    if (msg.startsWith("turkey")) {
+
         //Starting servers
         var j = 0;
-        for (x of serverList){
-            if(msg == 'turkey ' + x.toLowerCase() + ' start'){
-                if (fs.existsSync(x+'.txt') == false){
-                server.runServer(x, cfg.serverPath);
-                message.reply(x+ ' server started at '+ ipadd +':'+portList[j]);
-                }else{
-                    message.reply(x+ ' server already running at '+ ipadd +':'+portList[j]);
+        for (x of serverList) {
+            if (msg == 'turkey ' + x.toLowerCase() + ' start') {
+                if (fs.existsSync(x + '.txt') == false) {
+                    server.runServer(x, cfg.serverPath);
+                    message.reply(x + ' server started at ' + ipadd + ':' + portList[j]);
+                } else {
+                    message.reply(x + ' server already running at ' + ipadd + ':' + portList[j]);
                 }
                 return;
             }
@@ -62,22 +71,22 @@ bot.on('message', function(message){
         }
 
         //Help Command
-        if (msg == 'turkey help'){
-            for (x of serverList){
+        if (msg == 'turkey help') {
+            for (x of serverList) {
 
-                message.reply('Turkey '+x+ ' Start- To start a ' + x+ ' server')
+                message.reply('Turkey ' + x + ' Start- To start a ' + x + ' server')
                 console.log(msg);
             }
         }
         //status
         var j = 0;
-        if (msg == 'turkey status'){
-            for (x of serverList){
+        if (msg == 'turkey status') {
+            for (x of serverList) {
 
-                if (fs.existsSync(x+'.txt') == false){
-                    message.reply(x+' server is off');
-                } else{
-                    message.reply(x+' server is up and running at '+ ipadd +':'+portList[j]);
+                if (fs.existsSync(x + '.txt') == false) {
+                    message.reply(x + ' server is off');
+                } else {
+                    message.reply(x + ' server is up and running at ' + ipadd + ':' + portList[j]);
                 }
                 j++
             }
@@ -85,19 +94,19 @@ bot.on('message', function(message){
         }
 
         //Ram Usage
-        if(msg == 'turkey usage'){
+        if (msg == 'turkey usage') {
 
-            var mem = ((os.totalmem - os.freemem)/1024/1024/1024).toPrecision(3);
+            var mem = ((os.totalmem - os.freemem) / 1024 / 1024 / 1024).toPrecision(3);
 
 
-            message.reply('Ram ' + mem + 'GB/'+ (os.totalmem/1024/1024/1024).toPrecision(3)+'GB');
+            message.reply('Ram ' + mem + 'GB/' + (os.totalmem / 1024 / 1024 / 1024).toPrecision(3) + 'GB');
             console.log(msg);
         }
 
         //Get new IP
-        if (msg == 'turkey ip'){
+        if (msg == 'turkey ip') {
 
-            externalip(function (err, ip) {
+            publicIp.v4().then(ip => {
                 ipadd = (ip); // => 8.8.8.8
               });
 
@@ -107,3 +116,25 @@ bot.on('message', function(message){
     }
 
 });
+
+function StartAllServers() {
+    var j = 0;
+    for (x of serverList) {
+        if (fs.existsSync(x + '.txt') == false) {
+            server.runServer(x, cfg.serverPath);
+        }
+        j++;
+    }
+}
+
+rl.question('', (answer) => {
+    answer = answer.toLowerCase();
+    if(answer == 'start all'){
+        console.log('starting all servers:');
+        StartAllServers();
+    }
+    else {console.log('unknown command');
+}
+    //rl.close();
+  });
+
